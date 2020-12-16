3b94294a367e5e3ebf7a8969f3a3ab8a07a10c52
xiekai <xk19941011>
2020-12-16 09:20:09 +0800

    add MyLower UDF

--- hive-basic/src/main/java/com/xk/bigdata/hive/udf/lower/MyLower.java
+++ hive-basic/src/main/java/com/xk/bigdata/hive/udf/lower/MyLower.java
@@ -1,7 +1,17 @@
 package com.xk.bigdata.hive.udf.lower;
 
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.Text;
+
+/**
+ * MyLower
+ */
+@Description(name = "my_lower",
+        value = "_FUNC_(str) - returns lower str",
+        extended = "Example:\n"
+                + "  > SELECT _FUNC_('HADOOP') FROM src LIMIT 1;\n"
+                + "  'hadoop'")
 
 public class MyLower extends UDF {
     public Text evaluate(final Text s) {
